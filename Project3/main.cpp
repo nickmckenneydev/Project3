e5809cf4234d6fdca11ca896446fb72587a410ba
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-18 20:58:00 -0500

    Adding events

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,17 +1,14 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
+
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
 #include <cstdlib>
-int main(int argc, char* argv[])
-{
-	if (!SDL_Init(SDL_INIT_VIDEO)) {
-		SDL_Log("Failed!");
-		return EXIT_FAILURE;
-	}
 
-	SDL_Window* window = SDL_CreateWindow("HIIII", 320, 240, SDL_WINDOW_RESIZABLE|SDL_WINDOW_BORDERLESS);
-	SDL_Delay(5000);
-	SDL_Quit();
+	SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]);
 
-	return 0;
-}
+
+	SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event);
+	SDL_AppResult SDL_AppIterate(void* appstate);
+	void SDL_AppQuit(void* appstate, SDL_AppResult result);
