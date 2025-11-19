2ca1a7d2ee44a438a5abe1d1e40b00a63e0aa76a
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-18 21:08:10 -0500

    Added window with callback

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -6,9 +6,33 @@
 #include <SDL3/SDL_init.h>
 #include <cstdlib>
 
-	SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]);
+SDL_Window* window;
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+{
+	if (!SDL_Init(SDL_INIT_VIDEO))
+	{
+		return SDL_APP_FAILURE;
+	}
+	window = SDL_CreateWindow("wwwwww", 320, 240, SDL_WINDOW_RESIZABLE);
+	/*if (!window) {
+		SDL_GetError();
+	}*/
+	return SDL_APP_CONTINUE;
+}
 
 
-	SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event);
-	SDL_AppResult SDL_AppIterate(void* appstate);
-	void SDL_AppQuit(void* appstate, SDL_AppResult result);
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+	return SDL_APP_CONTINUE;
+}
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+		SDL_Delay(5000);
+		return SDL_APP_CONTINUE;
+}
+	
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+	SDL_Quit();
+}
