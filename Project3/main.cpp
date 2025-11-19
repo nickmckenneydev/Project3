3591e5ec814d7d9aed29932efdc0eca4cd73ffb2
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-18 21:12:01 -0500

    Moved back to simple main function

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -6,33 +6,13 @@
 #include <SDL3/SDL_init.h>
 #include <cstdlib>
 
-SDL_Window* window;
-
-SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
+int main()
 {
-	if (!SDL_Init(SDL_INIT_VIDEO))
-	{
-		return SDL_APP_FAILURE;
-	}
+	SDL_Init(SDL_INIT_VIDEO);
+	static SDL_Window* window;
 	window = SDL_CreateWindow("wwwwww", 320, 240, SDL_WINDOW_RESIZABLE);
-	/*if (!window) {
-		SDL_GetError();
-	}*/
-	return SDL_APP_CONTINUE;
-}
-
-
-SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
-{
-	return SDL_APP_CONTINUE;
-}
-SDL_AppResult SDL_AppIterate(void* appstate)
-{
-		SDL_Delay(5000);
-		return SDL_APP_CONTINUE;
-}
-	
-void SDL_AppQuit(void* appstate, SDL_AppResult result)
-{
+	SDL_Delay(5000);
 	SDL_Quit();
+	return 0;
 }
+
