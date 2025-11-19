90312c309aab19628012431b7f083b3e04648bbd
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-18 21:21:49 -0500

    Added event quit

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -5,14 +5,25 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_init.h>
 #include <cstdlib>
+#include <SDL3/SDL_events.h>
 
 int main()
 {
 	SDL_Init(SDL_INIT_VIDEO);
 	static SDL_Window* window;
 	window = SDL_CreateWindow("wwwwww", 320, 240, SDL_WINDOW_RESIZABLE);
-	SDL_Delay(5000);
-	SDL_Quit();
+
+	SDL_Event event;
+	
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_EVENT_QUIT:
+			SDL_Quit();
+		}
+		return SDL_APP_CONTINUE;
+	}
+
+	
 	return 0;
 }
 
