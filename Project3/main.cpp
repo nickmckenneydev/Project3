6a88f1c0e030e0159f1986141d79cf6d2759ae7e
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-19 21:07:39 -0500

    Got key to work

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,10 +1,4 @@
-#define SDL_MAIN_USE_CALLBACKS 1
-
-
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
-#include <SDL3/SDL_init.h>
-#include <cstdlib>
 #include <SDL3/SDL_events.h>
 
 int main()
@@ -13,20 +7,30 @@ int main()
 	static SDL_Window* window;
 	window = SDL_CreateWindow("wwwwww", 320, 240, SDL_WINDOW_RESIZABLE);
 
-	SDL_Event event;
-	SDL_zero(event);//init event quieie
-	event.type = event.key;
-	while (SDL_PollEvent(&event)) { //reading event from queue
-		switch (event.type) {
-		case SDL_EVENT_KEY_DOWN:
-			SDL_Quit();
-		default:
-			SDL_Log("Unhandled Event!");
-			break;
-		}
-		return SDL_APP_CONTINUE;
-	}
+	
+	
+	bool game_is_still_running = true;
 
+	while (game_is_still_running) {
+		SDL_Event event;
+		
+		SDL_zero(event);//init event quieie
+		while (SDL_PollEvent(&event)) { //reading event from queue. Repop event
+			switch (event.type) {
+			case SDL_EVENT_KEY_DOWN:
+				if (event.key.key == 'q') {
+					SDL_Log("We got a key event.");
+					game_is_still_running = false;
+					SDL_Quit();
+					break;
+				}
+			default:
+				SDL_Log("Unhandled Event!");
+			}
+			SDL_Log("Event queue empty.");
+			//return SDL_APP_CONTINUE;
+		}
+	}
 	
 	return 0;
 }
