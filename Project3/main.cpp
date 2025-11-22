fd057c13e7bce14f3edf87ce641a3169580341d1
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-21 19:16:03 -0500

    More cleanup

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -4,22 +4,21 @@
 struct SDLApplication {
 	SDL_Window* mWindow;
 	bool mGameRunning = true;
-	SDLApplication() {
+	SDLApplication(const char* title) {
 		SDL_Init(SDL_INIT_VIDEO);
-
-		mWindow = SDL_CreateWindow("Nick Window", 320, 240, SDL_WINDOW_RESIZABLE);
+		mWindow = SDL_CreateWindow(title, 320, 240, SDL_WINDOW_RESIZABLE);
 	}
 	~SDLApplication() {//Destructor
 		SDL_Quit();
 	}
-	void MainLoop() {
+	void Tick() {//Loop One Iteration
 		SDL_Event event;
-
-
-		while (mGameRunning) {
 			while (SDL_PollEvent(&event)) { //reading event from queue. Repop event
 				//SDL_Log("KEYBOARD STATE %p and % d\n", keys,*keys);
 				switch (event.type) {
+				case SDL_EVENT_QUIT:
+					mGameRunning = false;
+					break;
 				case SDL_EVENT_KEY_DOWN:
 					SDL_Log("We got a key event. :%d\n", event.key.key);
 					if (event.key.key == 'q') {
@@ -42,26 +41,27 @@ struct SDLApplication {
 						SDL_Log("button right clicked %d", event.button.button);
 					}
 					SDL_Log("We got a mouse q event. :%d\n", event.button.clicks);
-				case SDL_EVENT_QUIT:
-					mGameRunning = false;
+					break;
 				default:
 					SDL_Log("Unhandled Event!");
-				}
-
+				
 			}
 			float x, y;
 			SDL_MouseButtonFlags mouse = SDL_GetMouseState(&x, &y);
 			SDL_Log("x,y: %f,%f", x, y);
-
 		}
 	}
-	
+	void MainLoop() {
+		while (mGameRunning) {
+			Tick();
+		}
+	}
 };
 
 
-int main(int argc,char** argv)
+int main(int argc,char* argv[])
 {
-	SDLApplication app;
+	SDLApplication app("NICK NICK");
 	app.MainLoop();
 	return 0;
 }
