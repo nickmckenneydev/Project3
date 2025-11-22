550ff3c552db5f05cff81ce3cde98ee866f9e5f6
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-21 19:08:43 -0500

    Clean up main loop

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,56 +1,68 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_scancode.h>
-int main()
-{
-	SDL_Init(SDL_INIT_VIDEO);
-	static SDL_Window* window;
-	window = SDL_CreateWindow("wwwwww", 320, 240, SDL_WINDOW_RESIZABLE);
+struct SDLApplication {
+	SDL_Window* mWindow;
+	bool mGameRunning = true;
+	SDLApplication() {
+		SDL_Init(SDL_INIT_VIDEO);
 
-	int i = 50;
-	
-	bool game_is_still_running = true;
-	int w, h;
-	SDL_GetWindowSize(window, &w, &h);
+		mWindow = SDL_CreateWindow("Nick Window", 320, 240, SDL_WINDOW_RESIZABLE);
+	}
+	~SDLApplication() {//Destructor
+		SDL_Quit();
+	}
+	void MainLoop() {
+		SDL_Event event;
 
-	SDL_WarpMouseInWindow(window, w/2, h / 2);
-	float x, y;
 
-	SDL_Event event;
-	SDL_zero(event);//init event quieie
-	const bool *keys = SDL_GetKeyboardState(nullptr);//Return a pointer to an internal managed array
-	SDL_MouseButtonFlags mouse;
-
-	while (game_is_still_running) {
-		while (SDL_PollEvent(&event)) { //reading event from queue. Repop event
-			//SDL_Log("KEYBOARD STATE %p and % d\n", keys,*keys);
-			switch (event.type) {
-			case SDL_EVENT_KEY_DOWN:
-				SDL_Log("We got a key event. :%d\n", event.key.key);
-
-				if (event.key.key == 'q') {
-					SDL_Log("We got a key event.");
-					game_is_still_running = false;
-					SDL_Quit();
-					break;
+		while (mGameRunning) {
+			while (SDL_PollEvent(&event)) { //reading event from queue. Repop event
+				//SDL_Log("KEYBOARD STATE %p and % d\n", keys,*keys);
+				switch (event.type) {
+				case SDL_EVENT_KEY_DOWN:
+					SDL_Log("We got a key event. :%d\n", event.key.key);
+					if (event.key.key == 'q') {
+						SDL_Log("We got a key event.");
+						mGameRunning = false;
+						SDL_Quit();
+						break;
+					}
+				case SDL_EVENT_MOUSE_BUTTON_DOWN:
+					if (event.button.button == SDL_BUTTON_LEFT)
+					{
+						SDL_Log("button left clicked %d", event.button.button);
+					}
+					if (event.button.button == SDL_BUTTON_MIDDLE)
+					{
+						SDL_Log("button middle clicked %d", event.button.button);
+					}
+					if (event.button.button == SDL_BUTTON_RIGHT)
+					{
+						SDL_Log("button right clicked %d", event.button.button);
+					}
+					SDL_Log("We got a mouse q event. :%d\n", event.button.clicks);
+				case SDL_EVENT_QUIT:
+					mGameRunning = false;
+				default:
+					SDL_Log("Unhandled Event!");
 				}
-			case SDL_EVENT_MOUSE_BUTTON_DOWN:
-				SDL_Log("We got a mouse q event. :%d\n", event.button.clicks);
-				if (event.button.button == SDL_BUTTON_LEFT)
-				{
-					SDL_Log("button left clicked %d", event.button.button);
-				}
-			default:	
-				SDL_Log("Unhandled Event!");	
+
 			}
+			float x, y;
+			SDL_MouseButtonFlags mouse = SDL_GetMouseState(&x, &y);
+			SDL_Log("x,y: %f,%f", x, y);
 
 		}
-		mouse = SDL_GetMouseState(&x, &y);
-		SDL_Log("x,y %f %f %d", x, y,mouse);
-
 	}
-
 	
+};
+
+
+int main(int argc,char** argv)
+{
+	SDLApplication app;
+	app.MainLoop();
 	return 0;
 }
 
